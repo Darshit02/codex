b977611112c21f073e28ea6ab06aa42193649f75
LazY <darshitmistry2@gmail.com>
2024-04-09 09:31:34 +0530

    feat : create room using sessions

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -1,28 +1,28 @@
 import {
-    timestamp,
-    pgTable,
-    text,
-    primaryKey,
-   integer
-  } from "drizzle-orm/pg-core"
-  import type { AdapterAccount } from '@auth/core/adapters'
+  timestamp,
+  pgTable,
+  text,
+  primaryKey,
+  integer,
+  uuid,
+} from "drizzle-orm/pg-core";
+import type { AdapterAccount } from "@auth/core/adapters";
+import { sql } from "drizzle-orm";
 
+export const testing = pgTable("testing", {
+  id: text("id").notNull().primaryKey(),
+  name: text("name"),
+});
 
-export const testing = pgTable("testing",{
-    id : text("id").notNull().primaryKey(),
-    name : text("name").notNull(),
-})
+export const users = pgTable("user", {
+  id: text("id").notNull().primaryKey(),
+  name: text("name"),
+  email: text("email").notNull(),
+  emailVerified: timestamp("emailVerified", { mode: "date" }),
+  image: text("image"),
+});
 
-  
-  export const users = pgTable("user", {
-   id: text("id").notNull().primaryKey(),
-   name: text("name"),
-   email: text("email").notNull(),
-   emailVerified: timestamp("emailVerified", { mode: "date" }),
-   image: text("image"),
-  })
-  
-  export const accounts = pgTable(
+export const accounts = pgTable(
   "account",
   {
     userId: text("userId")
@@ -36,30 +36,48 @@ export const testing = pgTable("testing",{
     expires_at: integer("expires_at"),
     token_type: text("token_type"),
     scope: text("scope"),
-     id_token: text("id_token"),
+    id_token: text("id_token"),
     session_state: text("session_state"),
   },
   (account) => ({
-    compoundKey: primaryKey({ columns: [account.provider, account.providerAccountId] }),
+    compoundKey: primaryKey({
+      columns: [account.provider, account.providerAccountId],
+    }),
   })
-  )
-  
-  export const sessions = pgTable("session", {
-   sessionToken: text("sessionToken").notNull().primaryKey(),
-   userId: text("userId")
-     .notNull()
-     .references(() => users.id, { onDelete: "cascade" }),
-   expires: timestamp("expires", { mode: "date" }).notNull(),
+);
+
+export const sessions = pgTable("session", {
+  sessionToken: text("sessionToken").notNull().primaryKey(),
+  userId: text("userId")
+    .notNull()
+    .references(() => users.id, { onDelete: "cascade" }),
+  expires: timestamp("expires", { mode: "date" }).notNull(),
+});
+
+export const verificationTokens = pgTable(
+  "verificationToken",
+  {
+    identifier: text("identifier").notNull(),
+    token: text("token").notNull(),
+    expires: timestamp("expires", { mode: "date" }).notNull(),
+  },
+  (vt) => ({
+    compoundKey: primaryKey({ columns: [vt.identifier, vt.token] }),
   })
-  
-  export const verificationTokens = pgTable(
-   "verificationToken",
-   {
-     identifier: text("identifier").notNull(),
-     token: text("token").notNull(),
-     expires: timestamp("expires", { mode: "date" }).notNull(),
-   },
-   (vt) => ({
-     compoundKey: primaryKey({ columns: [vt.identifier, vt.token] }),
-   })
-  )
+);
+
+export const room = pgTable("room", {
+  id: uuid("id")
+    .default(sql`gen_random_uuid()`)
+    .notNull()
+    .primaryKey(),
+  userId: text("userId")
+    .notNull()
+    .references(() => users.id, { onDelete: "cascade" }),
+  name: text("name").notNull(),
+  description: text("description"),
+  language: text("language").notNull(),
+  githubRepo: text("githubRepo"),
+});
+
+export type Room = typeof room.$inferSelect;
